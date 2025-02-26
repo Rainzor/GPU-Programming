64f279253596f012f1e77102097f603e334b3d34
Rainzor <runze.wang02@outlook.com>
2025-02-26 21:24:32 +0800

    complete project3

--- 3-CUDA-Path-Tracer/src/core/sceneStructs.h
+++ 3-CUDA-Path-Tracer/src/core/sceneStructs.h
@@ -19,7 +19,7 @@
 #include "light.h"
 
 
-#define BACKGROUND_COLOR (glm::vec3(0.5f))
+#define BACKGROUND_COLOR (glm::vec3(0.2f))
 #define MIN(a,b) (((a) < (b)) ? (a) : (b))
 #define MAX(a,b) (((a) > (b)) ? (a) : (b))
 
